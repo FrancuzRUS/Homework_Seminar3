ed7e0921028758d413f8363c851cffb4b19268e8
Sergey Kasitsin <kasitsinsergey@gmail.com>
2022-05-22 12:39:22 +0300

    Task3 complited

---
+++ Task3/Program.cs
@@ -0,0 +1,13 @@
+﻿// 3. Найти сумму чисел от 1 до А
+
+Console.Write("Введите A: "); 
+int A = int.Parse(Console.ReadLine());
+
+int Sum = 0;
+int i = 0;
+
+for (i = 1; i <= A;  i ++) Sum = Sum+i ; 
+
+Console.WriteLine($"Сумма чисел равна {Sum}");
+    
+
