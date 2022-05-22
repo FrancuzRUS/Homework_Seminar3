8fd2de648ab7e7e928da8a67041a23358ed600de
Sergey Kasitsin <kasitsinsergey@gmail.com>
2022-05-22 12:30:58 +0300

    Task3 complite

---
+++ Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿// 2. Найти кубы чисел от 1 до N
+
+
+Console.Write("Введите N: "); 
+int N = int.Parse(Console.ReadLine());
+
+int Qube = 0;
+int i = 0;
+
+for (i = 1; i <= N;  i ++)
+    {Qube = i*i*i ; 
+    Console.Write($"Для числа {i} ");
+    Console.WriteLine($"Куб равен {Qube}");
+    }
+
+
