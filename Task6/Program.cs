49475d2d40d24b252ce9d65d1a795fac9dcc12eb
Sergey Kasitsin <kasitsinsergey@gmail.com>
2022-05-22 13:11:44 +0300

    Task6 Completed

---
+++ Task6/Program.cs
@@ -0,0 +1,19 @@
+﻿//6. Показать кубы чисел, заканчивающихся на четную цифру
+
+Console.Write("Введите четное число N: "); 
+int N = int.Parse(Console.ReadLine());
+int Qube = 0;
+
+if (N%2 != 0) Console.WriteLine("Вы ввели нечетное число!");
+
+else 
+{
+   Qube = N*N*N ; 
+    Console.WriteLine($"Куб равен {Qube}");
+}
+
+
+
+
+
+
