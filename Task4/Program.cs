da700db2fadab19ee51f47817579d0a5792e5915
Sergey Kasitsin <kasitsinsergey@gmail.com>
2022-05-22 12:49:45 +0300

    Task4 complited

---
+++ Task4/Program.cs
@@ -0,0 +1,20 @@
+﻿// 4. Возведите число А в натуральную степень B используя цикл
+
+Console.Write("Введите число А:");
+int A = int.Parse(Console.ReadLine());
+
+Console.Write("Введите число B:");
+int B = int.Parse(Console.ReadLine());
+
+int C = 1;
+int i = 1;
+
+for (i = 1; i <= B; i++) C = A*C; 
+Console.WriteLine($"Число А в степени В = {C}");
+
+
+
+
+
+
+
