31bf93510eb5690f63ba48a82acc8e81af6883de
Sergey Kasitsin <kasitsinsergey@gmail.com>
2022-05-22 12:25:50 +0300

    Task1 complited

---
+++ Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿// 1. Показать таблицу квадратов чисел от 1 до N
+
+Console.Write("Введите N: "); 
+int N = int.Parse(Console.ReadLine());
+
+int Square = 0;
+int i = 0;
+
+for (i = 1; i <= N;  i ++)
+    {Square = i*i ; 
+    Console.Write($"Для числа {i} ");
+    Console.WriteLine($"Квадрат равен {Square}");
+    }
+    
+
