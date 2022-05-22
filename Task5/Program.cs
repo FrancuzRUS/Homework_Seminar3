52322bcef532c1711bb61120d818e5510753a72a
Sergey Kasitsin <kasitsinsergey@gmail.com>
2022-05-22 12:59:20 +0300

    Task5 complited

---
+++ Task5/Program.cs
@@ -0,0 +1,17 @@
+﻿//5. Подсчитать сумму цифр в числе
+
+Console.WriteLine("Введите число");
+int N = int.Parse(Console.ReadLine());
+
+int Sum = 0; 
+
+while (N >= 1)
+{
+    Sum = Sum + N%10;
+    N = (N-N%10)/10;
+}
+ Console.WriteLine ($"Сумма цифр в числе равна  {Sum}");
+
+
+
+
